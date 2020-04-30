d022292fefc09a46995b86ea59e17e389c04d23b
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2020-04-30 14:52:21 +0530

    Removing unnecessary code

--- src/main/java/sample/PersonController.java
+++ src/main/java/sample/PersonController.java
@@ -23,11 +23,7 @@ public class PersonController {
 	@RequestMapping(path = "/person", method = RequestMethod.POST)
 	public Person person(@Valid @RequestBody Person person) {
 		 
-	     int nxt = ran.nextInt(10); 
-	     if(nxt>=5)
-	     {
-	    	 throw new RuntimeException("Breaking logic");
-	     }
+	    
 		return person;
 	}
 	@RequestMapping(path = "/personByLastName", method = RequestMethod.GET)
