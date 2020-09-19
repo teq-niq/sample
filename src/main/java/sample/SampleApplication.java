152099a259375ab06f1f80a0453f0d5c2fa0d9a3
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2020-09-19 18:50:51 +0530

    demonstrating the new simpler way of achieveing fqn

--- src/main/java/sample/SampleApplication.java
+++ src/main/java/sample/SampleApplication.java
@@ -1,13 +1,10 @@
 package sample;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -25,13 +22,7 @@ public class SampleApplication {
 	@Autowired 
 	ObjectMapper ojectMapper;
 	
-	@PostConstruct void init()
-	{
-		CustomConverter.add(ojectMapper);
-		//in future instead of above we should be able to use below
-		//will have to wait for a few releases in swagger and springdoc
-		//io.swagger.v3.core.jackson.TypeNameResolver.std.setUseFqn(true);
-	}
+	
 	
 	@Bean
 	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
