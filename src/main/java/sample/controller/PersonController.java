725c111bcba240724628bf6968d5302beb4693a5
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2021-08-23 21:22:10 +0530

    formatting

--- src/main/java/sample/controller/PersonController.java
+++ src/main/java/sample/controller/PersonController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonController {
 
-	@PostMapping(path = "/person", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@PostMapping(path = "/person", consumes = { MediaType.APPLICATION_JSON_VALUE, 
+			MediaType.APPLICATION_XML_VALUE })
 	public Person person(@Valid @RequestBody Person person) {
 		return person;
 	}
