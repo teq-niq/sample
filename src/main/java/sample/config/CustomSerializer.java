71ee48189c8162bd8f69d243b4209be169218ec8
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2021-09-21 18:10:57 +0530

    Making an improvement

--- src/main/java/sample/config/CustomSerializer.java
+++ src/main/java/sample/config/CustomSerializer.java
@@ -33,39 +33,16 @@ public class CustomSerializer extends SchemaSerializer {
             defaultSerializer.serialize(value, jgen, provider);
         } else {
             jgen.writeStartObject();
-            jgen.writeStringField("$ref", value.get$ref());
             String ref = value.get$ref();
             if(!ref.startsWith("#/components/schemas/"))
             {
             	ref="#/components/schemas/"+ref;
             	value.$ref(ref);
             }
-           
-            //copyExtensions(value, jgen);
+            jgen.writeStringField("$ref", value.get$ref());
             jgen.writeEndObject();
         }
     }
-    //copy extensions
-	//related to 
-	//https://github.com/swagger-api/swagger-core/pull/3539
-	//Complex types are handled by ref
-	//It would be convenient in spite of their being ref to show 
-	//constraints by schema extensions
 
-    protected void copyExtensions(Schema value, JsonGenerator jgen) throws IOException {
-        Map<String, Object> extensions = value.getExtensions();
-        if(extensions!=null) {
-            Set<String> extensionsKeySet = extensions.keySet();
-            for (String extensionKey : extensionsKeySet) {
-                Object extensionValue = extensions.get(extensionKey);
-                if(extensionValue!=null) {
-                   jgen.writeObjectField(extensionKey, extensionValue);
-                } else {
-                    jgen.writeNullField(extensionKey);
-                }
-            }
-
-        }
-    }
 
 }
