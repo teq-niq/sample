7f50a5585b4a853d44704233ce7f1e8689a9a9bc
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2024-03-05 21:19:40 +0530

    updated

--- src/main/java/sample/PersonController.java
+++ src/main/java/sample/PersonController.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
