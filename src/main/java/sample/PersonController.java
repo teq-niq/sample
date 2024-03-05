6c788da1b3d09192069e96e67b0d874e1595118b
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2024-03-05 20:10:29 +0530

    updated

--- src/main/java/sample/PersonController.java
+++ src/main/java/sample/PersonController.java
@@ -1,5 +1,5 @@
 package sample;
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
