501834522932f7e3cea39c4849d89b583be39f1c
Raghuraman Ramaswamy <raghuraman1@yahoo.com>
2024-03-11 18:21:20 +0530

    save

--- src/main/java/sample/SampleApplication.java
+++ src/main/java/sample/SampleApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
